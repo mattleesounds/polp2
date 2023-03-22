dce4cc6376ae28a63102b6cd0e0ed79fd76f02d0
mattleesounds <mattleesounds@gmail.com>
2023-03-22 08:27:45 -0500

    starting to put media in cloud

--- components/Media.tsx
+++ components/Media.tsx
@@ -1,53 +1,52 @@
-import React from 'react'
-import { TrackType } from '@/lib/types'
-import { useState, useRef } from 'react'
-import Feed from './Feed'
-import ControlBar from './ControlBar'
+import React from "react";
+import { TrackType } from "@/lib/types";
+import { useState, useRef } from "react";
+import Feed from "./Feed";
+import ControlBar from "./ControlBar";
 
-const Media = (): JSX.Element =>  {
+const Media = (): JSX.Element => {
   /* Tracks */
   const tracks: TrackType[] = [
     {
-      title: '50 Ways to Leave Your Lover',
-      artist: 'Paul Simon',
-      source: 'song1.mp3'
+      title: "50 Ways to Leave Your Lover",
+      artist: "Paul Simon",
+      source: "song1.mp3",
     },
     {
-      title: 'So Fresh, So Clean',
-      artist: 'Outkast',
-      source: 'song2.mp3'
+      title: "So Fresh, So Clean",
+      artist: "Outkast",
+      source:
+        "https://polp-media.s3.us-east-2.amazonaws.com/Outkast+-+So+Fresh%2C+So+Clean+(Official+HD+Video).mp3",
     },
     {
-      title: 'Prelude in E Minor',
-      artist: 'Chopin',
-      source: 'song3.mp3'
+      title: "Prelude in E Minor",
+      artist: "Chopin",
+      source: "song3.mp3",
     },
     {
-      title: 'Giant Steps',
-      artist: 'John Coltrane',
-      source: 'song4.mp3'
+      title: "Giant Steps",
+      artist: "John Coltrane",
+      source: "song4.mp3",
     },
     {
-      title: 'Set You Free',
-      artist: 'The Black Keys',
-      source: 'song5.mp3',
+      title: "Set You Free",
+      artist: "The Black Keys",
+      source: "song5.mp3",
     },
     {
-      title: 'Special Affair/Curse',
-      artist: 'The Internet',
-      source: 'song6.mp3',
-    }
+      title: "Special Affair/Curse",
+      artist: "The Internet",
+      source: "song6.mp3",
+    },
   ];
 
   /* States */
-  const [isPlaying, setIsPlaying] = useState<boolean>(false)
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [currentTrack, setCurrentTrack] = useState<string | null>(null);
 
   /* Map of audioRefs */
   const audioRefs = useRef<Map<string, HTMLAudioElement>>(new Map());
   const durRefs = useRef<Map<string, number>>(new Map());
-
-  
 
   /* Play/Pause Function */
   const handlePlayPause = (trackSource: string) => {
@@ -56,10 +55,10 @@ const Media = (): JSX.Element =>  {
       if (audioElement) {
         if (audioElement.paused) {
           audioElement.play();
-          setIsPlaying(true)
+          setIsPlaying(true);
         } else {
           audioElement.pause();
-          setIsPlaying(false)
+          setIsPlaying(false);
         }
       }
     } else {
@@ -69,7 +68,7 @@ const Media = (): JSX.Element =>  {
       const audioElement = audioRefs.current.get(trackSource);
       if (audioElement) {
         audioElement.play();
-        setIsPlaying(true)
+        setIsPlaying(true);
       }
       setCurrentTrack(trackSource);
     }
@@ -77,17 +76,17 @@ const Media = (): JSX.Element =>  {
 
   return (
     <div>
-      <Feed 
-          tracks={tracks}
-          setIsPlaying={setIsPlaying}
-          isPlaying={isPlaying}
-          currentTrack={currentTrack}
-          setCurrentTrack={setCurrentTrack}
-          handlePlayPause={handlePlayPause}
-          audioRefs={audioRefs}
-          durRefs={durRefs}
-        />
-      <ControlBar 
+      <Feed
+        tracks={tracks}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentTrack={currentTrack}
+        setCurrentTrack={setCurrentTrack}
+        handlePlayPause={handlePlayPause}
+        audioRefs={audioRefs}
+        durRefs={durRefs}
+      />
+      <ControlBar
         isPlaying={isPlaying}
         setIsPlaying={setIsPlaying}
         currentTrack={currentTrack}
@@ -98,7 +97,7 @@ const Media = (): JSX.Element =>  {
         tracks={tracks}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Media
+export default Media;
