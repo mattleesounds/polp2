61d2d2735727359ad6f62158b54c44680118b170
mattleesounds <mattleesounds@gmail.com>
2023-04-13 21:45:33 -0500

    u

--- components/Upload.tsx
+++ components/Upload.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Storage } from "aws-amplify";
-import { useState } from "react";
 import { Auth } from "aws-amplify";
 
-const uploadFile = async (file: File): Promise<string> => {
+const uploadFile = async (file: File, metadata: any): Promise<string> => {
   const { key } = await Storage.put(file.name, file, {
     contentType: file.type,
+    metadata,
   });
 
   return key;
 };
 
-const Upload = () => {
+const uploadImage = async (image: File): Promise<string> => {
+  const { key } = await Storage.put(image.name, image, {
+    contentType: image.type,
+  });
+
+  return key;
+};
+
+const UploadFile = () => {
   const [file, setFile] = useState<File | null>(null);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState<File | null>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const selectedFile = event.target.files && event.target.files[0];
@@ -21,31 +32,56 @@ const Upload = () => {
     }
   };
 
-  const handleUpload = async () => {
-    //////////////////////////
-    try {
-      const session = await Auth.currentSession();
-      console.log("User session data:", session);
-    } catch (error) {
-      console.error("Error getting user session data:", error);
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = event.target.files && event.target.files[0];
+    if (selectedFile) {
+      setImage(selectedFile);
     }
-    //////////////////////////
+  };
+
+  const handleUpload = async () => {
     if (!file) return;
 
     try {
-      const uploadedFileKey = await uploadFile(file);
+      // Metadata for the audio file
+      const metadata = {
+        title,
+        description,
+      };
+
+      // Upload audio file with metadata
+      const uploadedFileKey = await uploadFile(file, metadata);
       console.log("File uploaded:", uploadedFileKey);
+
+      // Upload track image if available
+      if (image) {
+        const uploadedImageKey = await uploadImage(image);
+        console.log("Image uploaded:", uploadedImageKey);
+      }
     } catch (error) {
-      console.error("File upload failed:", error);
+      console.error("File or image upload failed:", error);
     }
   };
 
   return (
     <div>
-      <input type="file" onChange={handleFileChange} />
+      <input
+        type="text"
+        placeholder="Track title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Track description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input type="file" accept="audio/*" onChange={handleFileChange} />
+      <input type="file" accept="image/*" onChange={handleImageChange} />
       <button onClick={handleUpload}>Upload</button>
     </div>
   );
 };
 
-export default Upload;
+export default UploadFile;
