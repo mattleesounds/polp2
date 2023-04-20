7afa0ded54b75c821beb4b9eaced20bd1a81b442
mattleesounds <mattleesounds@gmail.com>
2023-04-20 13:14:45 -0500

    u

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
 import awsExports from "../src/aws-exports.js";
 import { Auth } from "aws-amplify";
 import { Amplify } from "aws-amplify";
