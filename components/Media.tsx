c035cdcdd2cde43a692975b0e6773518684e00b5
mattleesounds <mattleesounds@gmail.com>
2023-04-20 22:13:19 -0500

    u

--- components/Media.tsx
+++ components/Media.tsx
@@ -1,12 +1,35 @@
 import React from "react";
-import { TrackType } from "@/lib/types";
-import { useState, useRef } from "react";
+//import { TrackType } from "@/lib/types";
+import { useState, useRef, useEffect } from "react";
 import Feed from "./Feed";
 import ControlBar from "./ControlBar";
+import { Storage, Auth } from "aws-amplify";
+
+interface TrackType {
+  title: string; // The title of the track
+  artist: string; // The name of the artist
+  source: string; // The source URL or path of the audio file
+}
+
+// Function to get the artist name by sub ID
+const getArtistNameBySubId = async (subId: string): Promise<string> => {
+  try {
+    // Get the user attributes from Cognito
+    const user = await Auth.userAttributes(
+      await Auth.currentAuthenticatedUser()
+    );
+    // Get the artist name from the "custom:Name" attribute
+    const artistName = user.find((attr) => attr.Name === "custom:Name")?.Value;
+    return artistName || "Unknown"; // Return 'Unknown' if the attribute is not found
+  } catch (error) {
+    console.error("Failed to get artist name:", error);
+    return "Unknown";
+  }
+};
 
 const Media = (): JSX.Element => {
   /* Tracks */
-  const tracks: TrackType[] = [
+  /* const tracks: TrackType[] = [
     {
       title: "50 Ways to Leave Your Lover",
       artist: "Paul Simon",
@@ -38,9 +61,10 @@ const Media = (): JSX.Element => {
       artist: "The Internet",
       source: "song6.mp3",
     },
-  ];
+  ]; */
 
   /* States */
+  const [tracks, setTracks] = useState<TrackType[]>([]);
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const [currentTrack, setCurrentTrack] = useState<string | null>(null);
 
@@ -48,31 +72,100 @@ const Media = (): JSX.Element => {
   const audioRefs = useRef<Map<string, HTMLAudioElement>>(new Map());
   const durRefs = useRef<Map<string, number>>(new Map());
 
-  /* Play/Pause Function */
-  const handlePlayPause = (trackSource: string) => {
-    if (currentTrack === trackSource) {
-      const audioElement = audioRefs.current.get(trackSource);
-      if (audioElement) {
-        if (audioElement.paused) {
-          audioElement.play();
-          setIsPlaying(true);
-        } else {
-          audioElement.pause();
-          setIsPlaying(false);
-        }
+  /* Fetch tracks from S3 bucket */
+  /* useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const items = (await Storage.list("public/media/", {
+          level: "public",
+        })) as unknown as { key: string }[];
+        const trackPromises = items
+          .filter(
+            (item) => item.key.endsWith(".mp3") || item.key.endsWith(".wav")
+          )
+          .map(async (item) => {
+            const fileName = item.key.split("/").pop() || "";
+            const title = fileName.split(".")[0];
+            const folderName = item.key.split("/").slice(-2, -1)[0] || "";
+            const subId = folderName.split("_")[0];
+            const artist = await getArtistNameBySubId(subId);
+            return {
+              title: title,
+              artist: artist,
+              source: item.key,
+            };
+          });
+        const trackList = await Promise.all(trackPromises);
+        setTracks(trackList);
+      } catch (error) {
+        console.error("Failed to fetch tracks:", error);
       }
-    } else {
-      audioRefs.current.forEach((audioElement) => {
-        audioElement.pause();
-      });
-      const audioElement = audioRefs.current.get(trackSource);
-      if (audioElement) {
-        audioElement.play();
-        setIsPlaying(true);
+    };
+    fetchTracks();
+  }, []); */
+
+  /*  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const result = await Storage.list("public/media/", { level: "public" });
+        console.log(result); // Log the result to the console
+
+        // Based on the structure of the result, extract the items array and process it
+        // For now, we'll leave this part empty until we know the structure of the result
+
+        // The rest of the code...
+      } catch (error) {
+        console.error("Failed to fetch tracks:", error);
       }
-      setCurrentTrack(trackSource);
-    }
-  };
+    };
+    fetchTracks();
+  }, []); */
+
+  /* Fetch tracks from S3 bucket */
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        // List track folders in the "public/media/" folder
+        const trackFolders = (await Storage.list("public/media/", {
+          level: "public",
+        })) as unknown as { key: string }[];
+
+        // Iterate over each track folder to retrieve audio files and metadata
+        const trackPromises = trackFolders.map(async (trackFolder) => {
+          const folderKey = trackFolder.key;
+          const folderContents = (await Storage.list(folderKey, {
+            level: "public",
+          })) as unknown as { key: string }[];
+
+          // Find the audio file in the folder contents
+          const audioFile = folderContents.find(
+            (item) => item.key.endsWith(".mp3") || item.key.endsWith(".wav")
+          );
+          if (!audioFile) return null; // Skip if no audio file found
+
+          // Extract metadata from the folder name
+          const folderName = folderKey.split("/").slice(-2, -1)[0] || "";
+          const subId = folderName.split("_")[0];
+          const title = folderName.split("_")[1];
+          const artist = await getArtistNameBySubId(subId);
+
+          return {
+            title: title,
+            artist: artist,
+            source: audioFile.key,
+          };
+        });
+
+        const trackList = (await Promise.all(trackPromises)).filter(
+          (track) => track !== null
+        ) as TrackType[];
+        setTracks(trackList);
+      } catch (error) {
+        console.error("Failed to fetch tracks:", error);
+      }
+    };
+    fetchTracks();
+  }, []);
 
   return (
     <div>
